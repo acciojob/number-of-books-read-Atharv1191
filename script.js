ac0161cd26c62ae082aac7d76414b57ae2c98ff7
Atharv1191 <119674561+Atharv1191@users.noreply.github.com>
2023-07-06 15:37:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Your Script here.
+ 
 const library = [
   {
     author: "Bill Gates",
@@ -15,11 +17,23 @@ const library = [
     readingStatus: false,
   },
 ];
-
+ 
 const numberOfBooksRead = () => {
   // write your code here
+	    // write your code here
+    let count=0;
+      library.forEach((i)=>{
+       if(i.readingStatus===true)
+       {
+         count++;
+       }
+      })
+      return count;
+ 
 };
-
+ 
 // Do not change the code below
-
+ 
 alert(numberOfBooksRead());
+ 
+
